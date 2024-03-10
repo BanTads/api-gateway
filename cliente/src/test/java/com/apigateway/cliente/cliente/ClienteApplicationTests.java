98c03504ab3942bb124bfab5435a3a4d69d8e2c2
Felipe Bomfim <felipebomfimdev@gmail.com>
2024-03-10 15:03:30 -0300

    Initial commit with microservices

---
+++ cliente/src/test/java/com/apigateway/cliente/cliente/ClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apigateway.cliente.cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ gerentes/src/test/java/com/apigateway/gerentes/gerentes/GerentesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apigateway.gerentes.gerentes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerentesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
