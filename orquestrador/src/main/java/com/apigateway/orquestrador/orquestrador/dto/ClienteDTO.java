fe015f6a283c7637b65a264fde737d662b79700c
felipehbomfim <lipe1800@hotmail.com>
2024-03-21 14:28:13 -0300

    DTOs que faltaram

---
+++ orquestrador/src/main/java/com/apigateway/orquestrador/orquestrador/dto/ClienteDTO.java
@@ -0,0 +1,17 @@
+package com.apigateway.orquestrador.orquestrador.dto;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+public class ClienteDTO implements Serializable {
+    private Long id;
+    private String nome;
+    private String email;
+    private String cpf;
+    private String telefone;
+    private float salario;
+    private EnderecoDTO endereco;
+}
